9956c70b6110272b38537dfc2122f12c8b66bc96
SR21C <rysawad@gmail.com>
2023-06-01 23:48:46 +0900

    [addデータ通信のテストオブジェクト追加

---
+++ NoAlly_ObjectTest/Assets/HTTP/Scripts/Outgame/Game/Title.cs
@@ -0,0 +1,54 @@
+﻿using MD;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Outgame
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Title : MonoBehaviour
+    {
+        public void Start()
+        {
+            MasterData.Instance.Setup(() => { }, true);
+        }
+
+        public void Login()
+        {
+            User user = User.Load();
+            string guid = "";
+            if (user != null)
+            {
+                guid = user.GUID;
+            }
+            GameAPI.API.Login(guid, LoginCallback);
+        }
+
+        void LoginCallback(APIResponceLogin data)
+        {
+            if (data.udid == null)
+            {
+                SceneManager.LoadScene("NewUser");
+            }
+            else
+            {
+                StartCoroutine("LoginSequence");
+            }
+        }
+
+        IEnumerator LoginSequence()
+        {
+            if (!MasterData.Instance.IsSetupComplete) yield return null;
+
+            //TODO:
+
+
+            SceneManager.LoadScene("Field");
+        }
+    }
+}
---
+++ NoAlly_ObjectTest/Assets/HTTP/Scripts/Outgame/Network/ServerRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.PackageManager.Requests;
+
+namespace Network
+{
+    [Serializable]
+    public class APIRequestBase
+    {
+        public string session;
+        public string token;
+    }
+
+    
+    [Serializable]
+    public class APIRequestLogin : APIRequestBase
+    {
+        public string udid;
+    }
+
+    [Serializable]
+    public class APIRequestUserCreate : APIRequestBase
+    {
+        public string name;
+    }
+}
