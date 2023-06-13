36763183991da1c7287c76b1dd067ec7644bb579
SR21C <rysawad@gmail.com>
2023-06-13 23:54:29 +0900

    [update]ミッション管理更新

---
+++ NoAlly_ObjectTest/Assets/Misson/Scripts/Missons/EliminateEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EliminateEnemy : MissonBase
+{
+    [SerializeField,Tooltip("敵の総数")]
+    int targetEnemyCount = 0;
+
+    public EliminateEnemy(int missonID, string missonName, string missonExplan)
+        : base(missonID, missonName, missonExplan)
+    {
+        
+    }
+
+    public override void JudgeMissonClear()
+    {
+        
+    }
+}
