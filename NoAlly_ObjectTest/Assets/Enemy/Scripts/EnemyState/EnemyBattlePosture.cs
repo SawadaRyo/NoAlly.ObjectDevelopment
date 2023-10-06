11a08fdd41eeb71950aaf73c056c602e1a5d15d2
SR21C <rysawad@gmail.com>
2023-10-06 18:51:52 +0900

    [update]敵のステート遷移を更新

--- NoAlly_ObjectTest/Assets/Enemy/Scripts/EnemyState/EnemyBattlePosture.cs
+++ NoAlly_ObjectTest/Assets/Enemy/Scripts/EnemyState/EnemyBattlePosture.cs
@@ -1,40 +1,49 @@
 //日本語コメント可
 using UniRx;
+using UnityEngine;
 using State = StateMachine<EnemyBase>.State;
 
 public class EnemyBattlePosture : State
 {
-    /// <summary>
-    /// 攻撃のアニメーションを再生する　例：エネミーの弾を撃つアニメーションを再生する
-    /// </summary>
-    protected virtual void PlayAttackAnimation() { }
-    /// <summary>
-    /// 攻撃の挙動を処理する　例：エネミーが弾を生成し発射する処理を実行する
-    /// </summary>
-    protected virtual void AttackBehaviour(EnemyAttackEnum attackEnum) { }
+    Interval currentInterval = null;
+    protected override void OnEnter(State prevState)
+    {
+        base.OnEnter(prevState);
+        if (Owner.Player.Value == null)
+        {
+            Owner.EnemyStateMachine.Dispatch((int)StateOfEnemy.Saerching);
+        }
+        else
+        {
+            Owner.ObjectAnimator.SetBool("InSight", true);
+        }
+    }
 
     protected override void OnUpdate()
     {
         base.OnUpdate();
-        PlayAttackAnimation();
+        if (currentInterval.IsCountUp())
+        {
+            Owner.EnemyStateMachine.Dispatch((int)StateOfEnemy.NormalAttack);
+            Debug.Log("攻撃だよ！");
+        }
     }
 
-    protected override void OnTranstion()
+    protected override void OnExit(State nextState)
     {
+        base.OnExit(nextState);
+        currentInterval.ResetTimer();
+    }
+
+    protected override void OnSetUpState()
+    {
+        currentInterval = new(Owner.Paramater._attackInterval);
         Owner.Player
-            .Where(_ => IsActive == true)
+            .Skip(1)
+            .Where(player => player == null && IsActive)
             .Subscribe(player =>
             {
-                if (player == null)
-                {
-                    Owner.EnemyStateMachine.Dispatch((int)StateOfEnemy.Saerching);
-                    Owner.ObjectAnimator.SetBool("InSight", false);
-                }
-            }).AddTo(Owner);
-        Owner.EnemyAttackEnum
-            .Subscribe(attack =>
-            {
-
+                Owner.EnemyStateMachine.Dispatch((int)StateOfEnemy.Saerching);
             }).AddTo(Owner);
     }
 }
--- NoAlly_ObjectTest/Assets/Enemy/Scripts/StateOfEnemy.cs
+++ NoAlly_ObjectTest/Assets/Enemy/Scripts/StateOfEnemy.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public enum StateOfEnemy : int
 {
-    Idle, //
-    Move, //移動
-    Saerching, //プレイヤーを捜索中
-    BattlePosture, //戦闘態勢
-    Attack, //攻撃
-    Death //死亡
+    None = -1,
+
+    Idle = 0, //基本
+
+    Move = 1, //移動
+
+    Saerching = 2, //プレイヤーを捜索中
+
+    BattlePosture = 3, //戦闘態勢
+
+    Attack = 4, //攻撃
+    NormalAttack = 41, //通常攻撃
+    LoopAttack = 42, //継続的に繰り返す攻撃
+
+    Death = 5 //死亡
 }
